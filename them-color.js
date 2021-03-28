3bb8ea78e58afb624aea128a66bd7d457cbb6d6a
Imron Rosyadi <mas.iyom@gmail.com>
2021-03-28 19:36:36 +0700

    Update them-color.js

--- them-color.js
+++ them-color.js
@@ -1,16 +1,30 @@
 module.exports = {
   whiteModeColor: {
-    textColor1: "#3737B9",
-    mainColor1: "#7f7fd5",
-    mainColor2: "#86a8e7",
-    mainColor3: "#91eac9",
+    textColor1: "#3d7e9a",
+    mainColor1: "#3a5f7d",
+    mainColor2: "#5a809e",
+    mainColor3: "#dae1e3",
   },
   darkModeColor: {
-    textColor1: "#bd93f9",
-    mainColor1: "#ff79c6",
-    mainColor2: "#bd93f9",
+    textColor1: "#3d7e9a",
+    mainColor1: "#3a5f7d",
+    mainColor2: "#5a809e",
     mainColor3: "#6272a4",
   },
 }
 
 // color setting
+//module.exports = {
+//  whiteModeColor: {
+//    textColor1: "#3737B9", //main link text color
+//    mainColor1: "#7f7fd5", //leftmost text color
+//    mainColor2: "#86a8e7", //rightmost text color
+//    mainColor3: "#91eac9", //line bottom text color
+//  },
+//  darkModeColor: {
+//    textColor1: "#bd93f9",
+//    mainColor1: "#ff79c6",
+//    mainColor2: "#bd93f9",
+//    mainColor3: "#6272a4",
+//  },
+//}
